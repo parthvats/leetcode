59ba3f2349b91179951fe68bf020d1f40f1c4714
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-03-14 12:15:52 +0900

    Update 138. Copy List with Random Pointer.cpp

--- CP Question Solutions/138. Copy List with Random Pointer.cpp
+++ CP Question Solutions/138. Copy List with Random Pointer.cpp
@@ -20,3 +20,41 @@ public:
 
     }
 };
+
+
+
+O(1) space complexity solution below:
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return head;
+        Node* cpy1 = head;
+        Node* cpy2 = head;
+        Node* temp;
+
+        while(head){
+            temp = head->next;
+            head->next = new Node(head->val);
+            head->next->next = temp;
+            head = temp;
+        }
+
+        while(cpy1){
+            cpy1->next->random = cpy1->random?cpy1->random->next:NULL;
+            cpy1 = cpy1->next->next;
+        }
+
+        Node* res = new Node(0);
+        Node* temp1 = res;
+
+        while(cpy2){
+            res->next = cpy2->next;
+            res = res->next;
+            cpy2->next = cpy2->next->next;
+            cpy2 = cpy2->next;
+        }
+
+        return temp1->next;
+    }
+};
