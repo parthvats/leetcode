32b10388a14516fe710fab12fb9b3a71dd86e919
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-03 11:20:44 +0900

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- CP Question Solutions/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ CP Question Solutions/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -21,3 +21,28 @@ public:
         return connect(preorder, inorder, 0, preorder.size()-1);
     }
 };
+
+//with using map
+class Solution {
+public:
+    int index = 0;
+    TreeNode* connect(vector<int>& preorder, vector<int>& inorder, int start, int end, map<int, int>& mp){
+        if(start>end) return NULL;
+        TreeNode* root = new TreeNode(preorder[index]);
+        int p_idx = mp[root->val];;
+        
+        index++;
+        root->left = connect(preorder, inorder, start, p_idx-1, mp);
+        root->right = connect(preorder, inorder, p_idx+1, end, mp);
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int, int> mp;
+
+        for(int i = 0; i < inorder.size(); i++){
+            mp[inorder[i]] = i;
+        }
+        return connect(preorder, inorder, 0, preorder.size()-1, mp);
+    }
+};
