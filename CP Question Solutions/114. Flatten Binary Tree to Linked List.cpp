efcdab11433ae13f7192982ebbe4d56a6d2ba3ac
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-05 14:33:01 +0900

    Update 114. Flatten Binary Tree to Linked List.cpp

--- CP Question Solutions/114. Flatten Binary Tree to Linked List.cpp
+++ CP Question Solutions/114. Flatten Binary Tree to Linked List.cpp
@@ -20,3 +20,19 @@ public:
         flatten(temp);
     }
 };
+
+// post order traversal solution
+class Solution {
+private: TreeNode* prev = NULL;
+public:
+    void flatten(TreeNode* root) {
+        if(root == NULL) return;
+        flatten(root->right);
+        flatten(root->left);
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
+    }
+};
+
+
