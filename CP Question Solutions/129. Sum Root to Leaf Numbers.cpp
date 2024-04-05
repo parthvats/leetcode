c44ec81e9dc69bda17f5a0f2d3850aade9e0e12a
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-05 15:15:48 +0900

    Update 129. Sum Root to Leaf Numbers.cpp

--- CP Question Solutions/129. Sum Root to Leaf Numbers.cpp
+++ CP Question Solutions/129. Sum Root to Leaf Numbers.cpp
@@ -17,3 +17,25 @@ public:
         return res;
     }
 };
+
+// with using stoi or to_string nethods
+
+class Solution {
+private: 
+    int res = 0;
+public:
+    void sum(TreeNode* root, int num){
+        if(root == NULL) return;
+        num += root->val;
+        if(root->left == NULL && root->right == NULL){
+            res += num;
+            return;
+        }
+        sum(root->left, num*10);
+        sum(root->right, num*10);
+    }
+    int sumNumbers(TreeNode* root) {
+        sum(root, 0);
+        return res;
+    }
+};
