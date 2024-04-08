8f26debd8ed2abce36c6ee579df5baded092bd38
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-08 14:45:35 +0900

    Update 236. Lowest Common Ancestor of a Binary Tree.cpp

--- CP Question Solutions/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ CP Question Solutions/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -1,3 +1,21 @@
+//Efficient way
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) return root;
+        if(p == root || q == root) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if(left == NULL && right == NULL) return NULL;
+        else if(left != NULL && right == NULL) return left;
+        else if(left == NULL && right != NULL) return right;
+        else return root;
+    }
+};
+
+//My sol
 class Solution {
     vector<TreeNode*> v1;
     vector<TreeNode*> v2;
