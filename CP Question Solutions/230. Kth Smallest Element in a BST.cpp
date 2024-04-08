3c97e64f7bdeb05f3e38a517402541b46447788b
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-08 17:23:22 +0900

    Update 230. Kth Smallest Element in a BST.cpp

--- CP Question Solutions/230. Kth Smallest Element in a BST.cpp
+++ CP Question Solutions/230. Kth Smallest Element in a BST.cpp
@@ -17,3 +17,28 @@ public:
         return 0;
     }
 };
+
+//Better solution as the tree is Binary Search Tree
+class Solution {
+private:
+    int count;
+    int res;
+public:
+    void kSmallest(TreeNode* root){
+        if(root == NULL) return;
+        if(count == 0) return;
+        kSmallest(root->left);
+        count--;
+        if(count == 0){
+            res = root->val;
+            return;
+        }
+        kSmallest(root->right);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        count = k;
+        kSmallest(root);
+        return res;
+    }
+};
