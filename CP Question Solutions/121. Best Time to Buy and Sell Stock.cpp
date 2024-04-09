d6356f9d68c2cf5d74cea460d1cd972f769a76db
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-09 15:56:42 +0900

    Update 121. Best Time to Buy and Sell Stock.cpp

--- CP Question Solutions/121. Best Time to Buy and Sell Stock.cpp
+++ CP Question Solutions/121. Best Time to Buy and Sell Stock.cpp
@@ -11,3 +11,23 @@ public:
         return res;
     }
 };
+
+// Kadane's algo
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int res = 0;
+        int sum = 0;
+
+        for(int i = 1; i < n; i++){
+            if(sum < 0){
+                sum = prices[i] - prices[i-1];
+            }
+            else sum += prices[i] - prices[i-1];
+            res = max(res, sum);
+        }
+        return res;
+    }
+};
