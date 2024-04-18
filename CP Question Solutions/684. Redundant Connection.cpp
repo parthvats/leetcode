ca0bbee617b39451b64020183668c03169ec1db8
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-18 10:01:05 +0900

    Update 684. Redundant Connection.cpp

--- CP Question Solutions/684. Redundant Connection.cpp
+++ CP Question Solutions/684. Redundant Connection.cpp
@@ -37,3 +37,43 @@ public:
         return {};
     }
 };
+
+
+
+// same solution logic as above but here adj matrix is vector<set<int>> instead of vector<vector<int>>
+class Solution {
+private:
+    vector<set<int>> adj;
+    vector<int> vis;
+    int right;
+public:
+    bool dfs(int x){
+        if(x == right) return true;
+        vis[x] = 1;
+        for(int i : adj[x]){
+            if(vis[i] == 1) continue;
+            if(dfs(i)) return true;
+        }
+        return false;
+        
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int m = edges.size();
+        adj.resize(m+1);
+
+        for(int i = 0; i < m; i++){
+            adj[edges[i][0]].insert(edges[i][1]);
+            adj[edges[i][1]].insert(edges[i][0]);
+        }
+
+        for(int i = m-1; i >= 0; i--){
+            right = edges[i][1];
+            adj[edges[i][0]].erase(right);
+            vis = vector<int>(m+1);
+            if(dfs(edges[i][0])) return edges[i];
+            adj[edges[i][0]].insert(right);
+            
+        }
+        return {};
+    }
+};
