fd6d5d3897fb14875f5b69e9ec707cbc047853b3
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-19 13:25:11 +0900

    Update Distance of nearest cell having 1.cpp

--- CP Question Solutions/Distance of nearest cell having 1.cpp
+++ CP Question Solutions/Distance of nearest cell having 1.cpp
@@ -56,3 +56,67 @@ class Solution
 	    return res;
 	}
 };
+
+
+
+// Good Solution
+
+class Solution 
+{
+    public:
+    //Function to find distance of nearest 1 in the grid for each cell.
+	vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    int m = grid.size();
+	    int n = grid[0].size();
+	    
+	    vector<vector<int>> vis(m, vector<int>(n,0));
+	    queue<pair<int, int>> q;
+	    
+	    for(int i = 0; i < m; i++){
+	        for(int j = 0; j < n; j++){
+	            if(grid[i][j] == 1){
+	                grid[i][j] = 0;
+	                vis[i][j] = 1;
+	                q.push({i, j});
+	            } 
+	        }
+	    }
+	    int res = 1;
+	    
+	    while(!q.empty()){
+	        int sz = q.size();
+	        
+	        for(int i = 0; i < sz; i++){
+	            pair<int, int> p = q.front();
+    	        int r = p.first;
+    	        int c = p.second;
+    	        q.pop();
+    	        if(r+1 < m && vis[r+1][c] == 0){
+    	            grid[r+1][c] = res;
+    	            vis[r+1][c] = 1;
+    	            q.push({r+1, c});
+    	        }
+    	        if(r-1 >= 0 && vis[r-1][c] == 0){
+    	            grid[r-1][c] = res;
+    	            vis[r-1][c] = 1;
+    	            q.push({r-1, c});
+    	        }
+    	        if(c+1 < n && vis[r][c+1] == 0){
+    	            grid[r][c+1] = res;
+    	            vis[r][c+1] = 1; 
+    	            q.push({r, c+1});
+    	        }
+    	        if(c-1 >= 0 && vis[r][c-1] == 0){
+    	            grid[r][c-1] = res;
+    	            vis[r][c-1] = 1;
+    	            q.push({r, c-1});
+    	        }
+    	        
+	        }
+	        res++;
+	    }
+	    
+	    return grid;
+	}
+};
