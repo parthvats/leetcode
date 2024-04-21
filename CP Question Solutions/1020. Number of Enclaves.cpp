b41423069bc0f727243c709e598a5a519b76dd60
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-21 10:29:31 +0900

    Update 1020. Number of Enclaves.cpp

--- CP Question Solutions/1020. Number of Enclaves.cpp
+++ CP Question Solutions/1020. Number of Enclaves.cpp
@@ -54,3 +54,55 @@ public:
         return count;
     }
 };
+
+
+// Better Solution
+
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int, int>> q;
+
+        for(int i = 0; i < m; i++) if(grid[i][0] == 1) q.push({i, 0});
+        for(int i = 0; i < m; i++) if(grid[i][n-1] == 1) q.push({i, n-1});
+        for(int i = 0; i < n; i++) if(grid[0][i] == 1) q.push({0, i});
+        for(int i = 0; i < n; i++) if(grid[m-1][i] == 1) q.push({m-1, i});
+
+        while(!q.empty()){
+            pair<int, int> p = q.front();
+            q.pop();
+            int r = p.first;
+            int c = p.second;
+
+            if(r+1 < m && grid[r+1][c] == 1){
+                grid[r+1][c] = 0;
+                q.push({r+1, c});
+            }
+            if(r-1 >= 0 && grid[r-1][c] == 1){
+                grid[r-1][c] = 0;
+                q.push({r-1, c});
+            }
+            if(c+1 < n && grid[r][c+1] == 1){
+                grid[r][c+1] = 0;
+                q.push({r, c+1});
+            }
+            if(c-1 >= 0 && grid[r][c-1] == 1){
+                grid[r][c-1] = 0;
+                q.push({r, c-1});
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 1; i < m-1; i++){
+            for(int j = 1; j < n-1; j++){
+                if(grid[i][j] == 1) count++;
+            }
+        }
+        return count;
+
+
+    }
+};
