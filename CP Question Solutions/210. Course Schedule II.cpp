d797734af18e0302c5428f7c31b57001e026780b
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-25 13:31:23 +0900

    Update 210. Course Schedule II.cpp

--- CP Question Solutions/210. Course Schedule II.cpp
+++ CP Question Solutions/210. Course Schedule II.cpp
@@ -1,3 +1,4 @@
+// this solution is based on Course Schedule question but the solution below is much faster and not exaclty based on Course Schedule 
 class Solution {
 private:
     vector<int> vis;
@@ -32,6 +33,52 @@ public:
         for(int i = 0; i < n; i++){
             if(path[i] == true) continue;
             if(!dfs(i, adj)) return {}; 
+        }
+
+        return res;
+    }
+};
+
+// better solution
+class Solution {
+private:
+    vector<int> vis;
+    vector<bool> path;
+    vector<int> res;
+    bool b = false;
+public:
+    void dfs(int v, vector<vector<int>>& adj){
+        if(vis[v] == 1) {
+            b= true;
+            return;
+        }
+        if(path[v] == true) return;
+        int sz = adj[v].size();
+        vis[v] = 1;
+        for(int i = 0; i < sz; i++){
+            dfs(adj[v][i], adj);
+            if(b) return;
+        }
+        path[v] = true;
+        vis[v] = 0;
+        res.push_back(v);
+    }
+
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(n);
+        int sz = pre.size();
+
+        for(int i = 0; i < sz; i++){
+            adj[pre[i][0]].push_back(pre[i][1]);
+        }
+
+        vis.resize(n, 0);
+        path.resize(n, false);
+
+        for(int i = 0; i < n; i++){
+            if(path[i] == true) continue;
+            dfs(i, adj);
+            if(b) return{};
         }
 
         return res;
