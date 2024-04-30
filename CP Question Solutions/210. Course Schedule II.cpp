dfdcd39a2bc418fcaf53d33802447eb1b22cb1b1
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-05-01 08:56:36 +0900

    Update 210. Course Schedule II.cpp

--- CP Question Solutions/210. Course Schedule II.cpp
+++ CP Question Solutions/210. Course Schedule II.cpp
@@ -84,3 +84,52 @@ public:
         return res;
     }
 };
+
+// by DFS khans algo
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(n);
+	    
+	    for(auto it : pre){
+	        adj[it[0]].push_back(it[1]);
+	    }
+	    
+	    vector<int> indeg(n, 0);
+	    
+	    for(auto it : adj){
+	        for(auto i : it){
+	            indeg[i]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    vector<int> res;
+	    stack<int> s;
+	    
+	    for(int i = 0; i < n; i++){
+	        if(indeg[i] == 0) {
+	            q.push(i);
+	        }
+	    }
+	    
+	    while(!q.empty()){
+	        int node = q.front();
+	        s.push(node);
+	        q.pop();
+	        for(auto it : adj[node]){
+	            indeg[it]--;
+	            if(indeg[it] == 0){
+	                q.push(it);
+	            }
+	        }
+	    }
+	    if(s.size() != n) return {};
+	    
+	    while(!s.empty()){
+	        res.push_back(s.top());
+	        s.pop();
+	    }
+	    return res;
+    }
+};
