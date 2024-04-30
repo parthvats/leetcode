d787a24c2b64010ccc0b8067cba394fd86f015f4
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-04-30 18:51:31 +0900

    Create Detect cycle in a directed graph using Khan's algo.cpp

---
+++ CP Question Solutions/Detect cycle in a directed graph using Khan's algo.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int n, vector<int> adj[]) {
+        vector<int> indeg(n, 0);
+        
+        for(int i = 0; i < n; i++){
+            for(auto it : adj[i]){
+                indeg[it]++;
+            }
+        }
+        
+        queue<int> q;
+        
+        for(int i = 0; i < n; i++){
+            if(indeg[i] == 0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it : adj[node]){
+                indeg[it]--;
+                if(indeg[it] == 0) q.push(it);
+            }
+        }
+        
+        for(auto it : indeg) {
+            if(it != 0) return true;
+        }
+        
+        return false;
+    }
+};
