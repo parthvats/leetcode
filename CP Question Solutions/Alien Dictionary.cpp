b62ddd8d99ae24ef90a27b85057d1b57d3b7466c
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-05-02 13:02:58 +0900

    Create Alien Dictionary.cpp

---
+++ CP Question Solutions/Alien Dictionary.cpp
@@ -0,0 +1,59 @@
+class Solution{
+    private:
+        map<char, set<char>> mp;
+        stack<int> s;
+        vector<int> path;
+    public:
+    
+    void comp(string s1, string s2, vector<vector<int>>& adj){
+        int i = 0;
+        int sz1 = s1.size();
+        int sz2 = s2.size();
+        
+        while(i < sz1 && i < sz2){
+            if(s1[i] == s2[i]) i++;
+            else {
+                adj[s1[i]-97].push_back(s2[i]-97);
+                return;
+            }
+        }
+    }
+    
+    void dfs(int v, vector<vector<int>>& adj){
+        if(path[v] == 1) return;
+        int sz = adj[v].size();
+        
+        for(int i = 0; i < sz; i++){
+            dfs(adj[v][i], adj);
+        }
+        
+        path[v] = 1;
+        s.push(v);
+    }
+    
+    
+    string findOrder(string dict[], int n, int k) {
+        
+        vector<vector<int>> adj(k);
+        
+        for(int i = 0; i < n-1; i++){
+            comp(dict[i], dict[i+1], adj);
+        }
+        
+        path.resize(k, 0);
+        
+        for(int i = 0; i < k; i++){
+            if(path[i] == 1) continue;
+            dfs(i, adj);
+        }
+        
+        string res;
+        
+        while(!s.empty()){
+            res.push_back(static_cast<char>(s.top()+97));
+            s.pop();
+        }
+        
+        return res;
+    }
+};
