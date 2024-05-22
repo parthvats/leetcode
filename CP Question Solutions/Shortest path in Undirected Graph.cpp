b341ca519a337d8d32176783ac5359cac029707d
Parth Vats <37781031+parthvats@users.noreply.github.com>
2024-05-22 10:09:14 +0900

    Update Shortest path in Undirected Graph.cpp

--- CP Question Solutions/Shortest path in Undirected Graph.cpp
+++ CP Question Solutions/Shortest path in Undirected Graph.cpp
@@ -31,3 +31,39 @@ class Solution {
         return dis;
     }
 };
+
+
+// Better SOlution
+class Solution {
+  public:
+    vector<int> shortestPath(vector<vector<int>>& e, int n,int m, int src){
+        vector<vector<int>> adj(n);
+        
+        for(int i = 0; i < m; i++){
+            adj[e[i][0]].push_back(e[i][1]);
+            adj[e[i][1]].push_back(e[i][0]);
+        }
+        
+        vector<int> vis(n, 0);
+        vector<int> dis(n, -1);
+        dis[src] = 0;
+        vis[src] = 1;
+        
+        queue<int> q;
+        q.push(src);
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            int sz = adj[node].size();
+            for(int i = 0; i < sz; i++){
+                if(vis[adj[node][i]] == 1) continue; 
+                q.push(adj[node][i]);
+                dis[adj[node][i]] = dis[node]+1;
+                vis[adj[node][i]] = 1;
+            }
+        }
+        
+        return dis;
+    }
+};
